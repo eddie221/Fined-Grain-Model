6afeb1a773dd81e5663841cfa73691a19d3a232c
Eddie <eddie1998221@gmail.com>
2021-02-21 16:53:43 +0800

    20210221-2

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ import tqdm
 
 #print environment information
 print(torch.cuda.is_available())
-DEVICE = 'cuda:0'
+DEVICE = 'cuda:1'
 
 #writer = SummaryWriter('../tensorflow/logs/cub_{}'.format(INDEX), comment = "224_64")
 
